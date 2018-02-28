5ecd3b3c03681f4e8dc091aa28a3c9b532585325
Ovv <contact@ovv.wtf>
2018-02-28 15:48:15 +0100

    python test

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,11 @@
 import requests
 
 r = requests.get('http://localhost:80')
+print(r.status_code)
 assert r.status_code == 301
 
 r = requests.get('http://localhost:80/admin/')
+print(r.status_code)
 assert r.status_code == 301
 
 r = requests.get('http://localhost:80/admin/login/?next=/admin/')
